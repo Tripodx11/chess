e9f273be366536b0d3bc34ebfe9e0d84bcffcd21
Teddy <teddyalanwright@gmail.com>
2025-06-30 16:20:06 -0600

    Changed some redundant code in bishop, rook, and queen move calculators and changed a call to piece type when calling our calculator functions for better code.

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -7,17 +7,11 @@ public class QueenMovesCalculator implements PieceMoveCalculator{
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
         Collection<ChessMove> validMoves = new ArrayList<>();
-
-        //Up and left diagonal
-        int startRow = position.getRow();
-        int startCol = position.getColumn();
-        int newRow = startRow;
-        int newCol = startCol;
         int [][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}, {1,-1}, {1,1}, {-1,1}, {-1,-1}};
 
         for (int[] direction : directions) {
-            newRow = startRow;
-            newCol = startCol;
+            int newRow = position.getRow();
+            int newCol = position.getColumn();
             while (true) {
                 newRow = newRow + direction[0];
                 newCol = newCol + direction[1];
