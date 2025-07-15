14705f829706cb4c433801073dc73db9e7d789aa
Teddy <teddyalanwright@gmail.com>
2025-07-14 20:02:16 -0600

    Had a hard time getting the server running. Got it to work. Had to move around folders and set resources and root directories and what not. Also added package folders for the server.

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,29 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+        Spark.get("/", (req, res) -> {
+            res.redirect("/index.html");
+            return null;
+        });
+
+        // Register your endpoints and handle exceptions here.
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
