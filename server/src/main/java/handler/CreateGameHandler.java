11defea441a4e1f54afd914e2e431f6fd0f13aa5
Teddy <teddyalanwright@gmail.com>
2025-07-16 13:06:00 -0600

    Got Create game handler built and added the code to the server for it. Untested

---
+++ server/src/main/java/handler/CreateGameHandler.java
@@ -0,0 +1,54 @@
+package handler;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import service.CreateGameService;
+import service.requests.CreateGameRequest;
+import service.results.CreateGameResult;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class CreateGameHandler implements Route {
+
+    private final DataAccess dataAccess;
+    private final Gson gson = new Gson();
+
+    public CreateGameHandler(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+
+        try {
+            String gameName = gson.fromJson(request.body(), CreateGameRequest.class).getGameName();
+            String authToken = request.headers("authorization");
+            CreateGameRequest createGameRequest = new CreateGameRequest(authToken, gameName);
+            CreateGameService createGameService = new CreateGameService(dataAccess);
+            CreateGameResult result = createGameService.createGame(createGameRequest);
+
+            if (result.getMessage() != null) {
+                if (result.getMessage().contains("unauthorized")) {
+                    response.status(401);
+                } else if (result.getMessage().contains("bad request")) {
+                    response.status(400);
+                } else {
+                    response.status(500);
+                }
+                return gson.toJson(new CreateGameHandler.ErrorResponse("Error: " + result.getMessage()));
+            }
+
+            response.status(200);
+            return gson.toJson(result);
+
+        } catch (Exception e) {
+            response.status(500);
+            return gson.toJson(new CreateGameHandler.ErrorResponse("Error: " + e.getMessage()));
+        }
+    }
+
+    private record ErrorResponse(String message) {}
+}
+
+
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,10 +2,7 @@ package server;
 
 import dataaccess.DataAccess;
 import dataaccess.SystemDataAccess;
-import handler.ClearHandler;
-import handler.LoginHandler;
-import handler.LogoutHandler;
-import handler.RegisterHandler;
+import handler.*;
 import spark.*;
 
 public class Server {
@@ -26,6 +23,7 @@ public class Server {
         Spark.post("/user", new RegisterHandler(dataAccess));
         Spark.post("/session", new LoginHandler(dataAccess));
         Spark.delete("/session", new LogoutHandler(dataAccess));
+        Spark.post("/game", new CreateGameHandler(dataAccess));
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
