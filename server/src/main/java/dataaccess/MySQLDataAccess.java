70378096d3ebc702b62a0954c648e4e863398e00
Teddy <teddyalanwright@gmail.com>
2025-07-22 17:17:00 -0600

    Added stubs tp MySQLDataAccess

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -1,9 +1,47 @@
 package dataaccess;
 
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Map;
+
 public class MySQLDataAccess implements DataAccess{
 
     public MySQLDataAccess() throws DataAccessException {
         DatabaseManager.createDatabase();
         DatabaseManager.createTables();
     }
+
+    public void clear() {};
+
+    //methods for adding data to models
+    public void addUser (UserData data) throws DataAccessException {};
+    public void addAuth (AuthData data) throws DataAccessException {};
+    public void addGame (GameData data) throws DataAccessException {};
+
+    //methods for getting data from keys
+    public UserData getUserData(String username) {
+        return null;
+    };
+
+    public AuthData getAuthTokenUN(String authToken) {
+        return null;
+    };
+
+    public GameData getGameData(int gameID) {
+        return null;
+    };
+
+    //unique game methods
+    public Map<Integer, GameData> getAllGameData() {
+        return null;
+    };
+
+    public int updateGameID() {
+        return -2;
+    };
+
+    //remove methods
+    public void removeAuthData (String authToken) {};
 }
